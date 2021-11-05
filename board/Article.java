ad09cb14d38c5b684b559224b724311468d8c8e8
minjg0392 <abb0392@naver.com>
2021-11-06 02:04:10 +0900

    coment, like modify

--- board/Article.java
+++ board/Article.java
@@ -3,11 +3,11 @@ package board;
 public class Article {
 	
 	//객체를 만든는 설계도
-	int no;
-	String title;
-	String writer;
-	String body;
-	String regDate;
+	private int no;
+	private String title;
+	private String writer;
+	private String body;
+	private String regDate;
 		
 	Article(int no, String title, String body, String regDate, String writer) {
 			this.no = no;
@@ -17,4 +17,47 @@ public class Article {
 			this.regDate = regDate;
 			
 	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getWriter() {
+		return writer;
+	}
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public String getRegDate() {
+		return regDate;
+	}
+
+	public void setRegDate(String regDate) {
+		this.regDate = regDate;
+	}
+	
+	
+
 }
